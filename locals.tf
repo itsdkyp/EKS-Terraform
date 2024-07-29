f50e0f7945376bc082fd0445d7399075a419a074
Dileep Kumar <itsdkyp@gmail.com>
2024-07-30 01:06:23 +0530

    added vpc and igw

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
     env          = "staging"
-    region       =  "us-east-1"
+    region       = "us-east-1"
     zone1        = "us-east-1a"
     zone2        = "us-east-1b"
     eks_name     = "demo"
