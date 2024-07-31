6bc8528c485fe3d41a9384f6828c5ba726707c70
Dileep Kumar <dileep.kumar.ext@juspay.in>
2024-07-31 18:14:04 +0530

    gitignore

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
-    env          = "staging"
-    region       = "us-east-1"
-    zone1        = "us-east-1a"
-    zone2        = "us-east-1b"
-    eks_name     = "test"
-    eks_version  = "1.29"
+  env         = "staging"
+  region      = "us-east-1"
+  zone1       = "us-east-1a"
+  zone2       = "us-east-1b"
+  eks_name    = "test"
+  eks_version = "1.29"
 }
